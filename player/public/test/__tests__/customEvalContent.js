ae46656955a4453ff0c83397900b5a38275a596c
Rahul Shukla <rahul.shukla13@gmail.com>
2020-03-13 15:29:28 +0530

    Issue #SB-0000 chore: Github action runner for functional test cases and git reporter

--- player/public/test/__tests__/customEvalContent.js
+++ player/public/test/__tests__/customEvalContent.js
@@ -1,5 +1,4 @@
 const timeout = 50000
-
 const typingSpeed = 50
 
 /**
@@ -7,10 +6,18 @@ const typingSpeed = 50
  * @todo To use image diff we need to ignore options container in MCQ cause it always shuffle 
  * @link https://github.com/americanexpress/jest-image-snapshot#ignoring-parts-of-the-image-snapshot-if-using-puppeteer
  */
-
-const { toMatchImageSnapshot } = require('jest-image-snapshot'); 
+const path = require('path')
+const { configureToMatchImageSnapshot } = require('jest-image-snapshot'); 
+const customConfig = { 
+    threshold: 0.9,
+    // failureThresholdType: 'percent',
+    // updatePassedSnapshot: true,
+}
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+    customDiffConfig: customConfig,
+    noColors: true
+})
 expect.extend({ toMatchImageSnapshot });
-
 
 describe(
     'Sunbird Player functional testing - Custom EVAL content',
@@ -46,30 +53,37 @@ describe(
 
         it('Player should load custom eval content', async () => {
             const playContent = await page.$('body > div:nth-child(7) > div > ion-pane > ion-content > div > div:nth-child(3)');
+            console.clear = console.log = console.warn = console.error = () => {};
             await playContent.click()
+            
         })
 
         it('custom eval content test with snapshot', async () => {
             const image = await page.screenshot();
-            // expect(image).toMatchImageSnapshot();
+            expect(image).toMatchImageSnapshot();
         })
 
         it('Custom eval without clicking on options opens popup', async () => {
+            page.on("console", msg => console.log("PAGE LOG:", msg));
             await page.waitForSelector('#gameCanvas')
             const nextNavigation = await page.waitForSelector('#overlay > next-navigation > div > div > a > img')
             await nextNavigation.click()
             const image = await page.screenshot();
-            // expect(image).toMatchImageSnapshot();
+            expect(image).toMatchImageSnapshot();
         })
 
         it('On click of try again it should stay in same page', async () => {
             const tryAgain = await page.waitForSelector('#popup-buttons-container > div.right.primary.button.ng-binding')
+            const image = await page.screenshot();
+            expect(image).toMatchImageSnapshot();
             await tryAgain.click();
         })
 
         it('Custom eval without clicking on options opens popup', async () => {
             await page.waitForSelector('#gameCanvas')
             const nextNavigation = await page.waitForSelector('#overlay > next-navigation > div > div > a > img')
+            const image = await page.screenshot();
+            expect(image).toMatchImageSnapshot();
             await nextNavigation.click()
         })
 
