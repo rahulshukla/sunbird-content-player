fb35c2621989eefcc0da7ea15b08a085d86f4c67
Rahul Shukla <rahul.shukla13@gmail.com>
2020-06-03 17:57:51 +0530

    Issue #SB-18100 chore: Remove security vulnerability from @project-sunbird/content-player

--- player/webpack.config.js
+++ player/webpack.config.js
@@ -21,7 +21,7 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+const ngAnnotatePlugin = require('babel-plugin-angularjs-annotate');
 const fs = require('fs');
 const replace = require('replace-in-file');
 const file_extra = require('fs-extra')
@@ -240,9 +240,9 @@ module.exports = (env, argv) => {
                 replaceStringInFiles(env.channel);
                 copyCorePlugins(env.channel);
             }),
-            // new ngAnnotatePlugin({
-            //     add: true,
-            // }),
+            new ngAnnotatePlugin({
+                explicitOnly: true,
+            }),
             new webpack.ProvidePlugin({
                 "window.$": "jquery",
                 "window._": 'underscore',
